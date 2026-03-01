cf00860f71bf66b64275e62f97386d4c25d45712
Felipe Castro <seu_email_no_github@example.com>
2026-03-01 17:33:52 -0300

    refactor miniola.py: adjust vertical and horizontal crop limits for improved processing

--- miniola_py/miniola.py
+++ miniola_py/miniola.py
@@ -39,8 +39,8 @@ ROI_X, ROI_Y = 175, 70
 ROI_W, ROI_H = 300, 45  
 LINHA_X, MARGEM = 400, 15
 THRESH_VAL = 110
-CROP_Y1, CROP_Y2 = 200, 300  # Ajuste o corte vertical
-CROP_X1, CROP_X2 = 60, 740  # Ajuste o corte horizontal (elimina bordas pretas)
+CROP_Y1, CROP_Y2 = 100, 500  # Ajuste o corte vertical
+CROP_X1, CROP_X2 = 50, 750  # Ajuste o corte horizontal (elimina bordas pretas)
 
 # Estado Global
 contador_perf = 0
